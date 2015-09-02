290b180d1f44afc237ba02d792f7187f330caa61
王思远 <wsy2355883@163.com>
2015-09-02 16:19:20 +0800

    开发

--- app.js
+++ app.js
@@ -12,22 +12,22 @@ app.engine('.html', require('ejs').__express);
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'html');
 app.use("/",express.static(path.join(__dirname, 'app')));
-//�м����https://github.com/expressjs/body-parser
+//https://github.com/expressjs/body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//��httpSession ���� mongodb
+//httpSession mongodb
 app.use(session({
   secret: 'foo',
   store: new MongoStore({
     // Basic usage
-    host: '127.0.0.1', // Default, optional
+    host: '123.56.94.37', // Default, optional
     port: 27017, // Default, optional
     db: 'wsy', // Required
     collection: 'sessions',
     // Basic authentication (optional)
-    username: 'wangsiyuan',
-    password: '123456',
+    //username: 'wangsiyuan',
+    //password: '123456',
 
     // Advanced options (optional)
     autoReconnect: true, // Default
@@ -45,7 +45,7 @@ app.get('/index', function(req, res, next) {
   console.log(req.sessionID);
   console.log(req.session.userInfo);
   if(!req.session.userInfo) {
-    res.redirect('/login');
+    res.redirect('login');
   } else {
     res.render('index');
   }
